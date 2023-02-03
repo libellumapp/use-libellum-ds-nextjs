31b7ff69e62fa32d29f9995a0512d73dee9d0f77
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2023-02-03 09:39:21 -0300

    project creation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Manrope } from '@next/font/google'
+
+import globalStyles from '@/styles/global'
+
+const manrope = Manrope({
+  weight: ['500', '600', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  globalStyles()
+  return  <>
+    
+      <main className={manrope.className}>
+        <Component {...pageProps} />
+      </main>  
+  </>
 }
